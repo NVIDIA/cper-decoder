fc361292ee1b887dd42071dbaaa1c514b4efea38
Karthik Rajagopalan <krajagopalan@nvidia.com>
2024-08-21 13:50:03 -0700

    Fix coverity issue on incorrect sizeof usage
    Fixes nvbug https://nvbugspro.nvidia.com/bug/4814851o
    
    Signed-off-by: Karthik Rajagopalan <krajagopalan@nvidia.com>

--- main.cpp
+++ main.cpp
@@ -99,7 +99,7 @@ string convertToStringAscii(unsigned char input[], int size)
  * @param input in the form of a string
  * @return corresponding integer value
  */
-int stringToInt(string input)
+int stringToInt(const string& input)
 {
     int result = 0;
     for (int i = 0; i < (int)input.size(); i++)
@@ -116,12 +116,12 @@ int stringToInt(string input)
  * This function takes a pointer to the timestamp array and formats it into a
  * string of a specific format NOTE: To be changed based on Redfish format
  * @param temp input to the function which is a pointer to the timestamp char
- * array
+ * array @param size Defines the size of the array passed
  * @return result which is the formatted string
  */
-string timestampDecode(unsigned char* temp)
+string timestampDecode(unsigned char* temp, int size)
 {
-    string time = convertToHex(temp, sizeof(temp));
+    string time = convertToHex(temp, size);
     string result = "Format: MM-DD-YYYY HH:MM:SS ";
 
     // Extract year, month, and day from the timestamp string
@@ -219,7 +219,7 @@ long int charToIntLittleEndian(unsigned char* input, int size)
  * @param input to the function which is a string
  * @return result which is the formatted string
  */
-string guidDecode(string input)
+string guidDecode(const string& input)
 {
     string result;
     for (int i = 6; i >= 0; i = i - 2)
@@ -713,7 +713,7 @@ void pcieErrPortTypeDecode(int input, int i)
  * @param input is the string which consisting of an address
  * @return string
  */
-string addressRepresentation(string input)
+string addressRepresentation(const string& input)
 {
     string x("0x");
     x.append(input);
@@ -725,28 +725,40 @@ int main(int argc, char** argv)
     // initialize hash maps
     initMaps();
     CLI::App app{"CPER Decoder"}; // label the command line interface
+
     string inputFile;
-    app.add_option(
-        "--redfish", inputFile,
-        "Binary File Path"); // provide an input option for the binary blob path
     string outputFile;
-    app.add_option(
-        "--json", outputFile,
-        "JSON File Path"); // provide an output option for the JSON file
+    try
+    {
+        // input option for the binary blob path
+        app.add_option("--redfish", inputFile, "Binary File Path");
+        // output option for the JSON file
+        app.add_option("--json", outputFile, "JSON File Path");
+    }
+    catch (CLI::Error& e)
+    {
+        return app.exit(e);
+    }
     CLI11_PARSE(app, argc, argv);
+
     ifstream binFile;
-    ofstream jsonFile;
-    string line;
     binFile.open(inputFile, ios::in | ios::binary); // open file in read mode
+    if (!binFile.is_open())
+        return app.exit(
+            CLI::FileError::Missing(inputFile + " was not readable"));
+
     ostringstream ostrm;
     ostrm << binFile.rdbuf();      // read the entire input binary file into an
                                    // output stringstream
-    line = ostrm.str();            // convert from type ostringstream to string
+    string line = ostrm.str();     // convert from type ostringstream to string
 
     header* p = (header*)&line[0]; // read the file into the header structure to
                                    // be able to segregate fields
 
-    jsonFile.open(outputFile);
+    ofstream jsonFile(outputFile);
+    if (!jsonFile.is_open())
+        return app.exit(
+            CLI::FileError::Missing(outputFile + " failed to open"));
 
     // Write the header fields into the JSON file
     string revision = convertToString(p->Revision, sizeof(p->Revision));
@@ -778,7 +790,8 @@ int main(int argc, char** argv)
     j["Header"]["RecordLength"] =
         charToIntLittleEndian(p->RecordLength, sizeof(p->RecordLength));
     string time = convertToHex(p->Timestamp, sizeof(p->Timestamp));
-    j["Header"]["Timestamp"] = timestampDecode(p->Timestamp);
+    j["Header"]["Timestamp"] = timestampDecode(p->Timestamp,
+                                               sizeof(p->Timestamp));
     j["Header"]["PlatformID"] =
         guidDecode(convertToHex(p->PlatformID, sizeof(p->PlatformID)));
     j["Header"]["PartitionID"] =
